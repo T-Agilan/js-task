41f657a258d99de823b5e0ef9a41e2d4e1459f40
T-Agilan <agilanakil1432@gmail.com>
2023-07-25 14:41:23 +0530

    objects using function

---
+++ object.js
@@ -0,0 +1,12 @@
+let str = { name: "BlackFlux",  number: 9597465677 } 
+let fun = { id: " 2 ",  Status: "Active", title: " IT start-up " }
+  function print (){
+    let a = Object.keys(str);
+    let b = Object.values(str);
+    let x = Object.keys(fun);
+    let y = Object.values(fun);
+    console.log(a,b)
+    console.log(x,y)
+  }
+  print()
+
