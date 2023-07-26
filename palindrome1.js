078498963cd559d82676adcf8883724a2b650a2c
T-Agilan <agilanakil1432@gmail.com>
2023-07-26 12:41:00 +0530

    deleted

--- palindrome1.js
+++
@@ -1,8 +0,0 @@
-let str="agilan";
-checkPalindrome(str);
-function checkPalindrome(str) {
-    const x = str.split('');
-    const y = x.reverse();
-    const z = y.join('');
-    (str===z)?console.log("true"):console.log("false");
-  }
